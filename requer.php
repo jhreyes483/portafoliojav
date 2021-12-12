8cbaa4100aa5e89dd4f91fcdf811b70ff85f76ed
Javier Reyes <jhreyes483@misena.edu.co>
2021-12-11 20:39:10 -0500

    dd

--- requer.php
+++ requer.php
@@ -22,7 +22,7 @@ $req = $obj->acces();
    <link href="template/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
     -->
    <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
-   <link href="template/css/sb-admin-2.min.css" rel="stylesheet">
+   <link href="public/css/sb-admin-2.min.css" rel="stylesheet">
    <script src="https://jfa.herokuapp.com/sicloud/public/layout1/js/fontawasome-ico.js"></script>
    <script src="public/js/jquery-3.5.1.min.js"></script>
    <script src="public/js/searchmain.js"></script>
