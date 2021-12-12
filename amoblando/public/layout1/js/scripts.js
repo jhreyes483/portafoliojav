bc0b69db3e6979f85aea7a6d4e4b0a0056209a2c
Javier Reyes <jhreyes483@misena.edu.co>
2021-12-11 21:18:53 -0500

    sd

--- amoblando/public/layout1/js/scripts.js
+++ amoblando/public/layout1/js/scripts.js
@@ -42,6 +42,8 @@
     });
 
     // Collapse Navbar
+
+     /*
     var navbarCollapse = function () {
         if ($("#mainNav").offset().top > 100) {
             $("#mainNav").addClass("navbar-shrink");
@@ -49,8 +51,9 @@
             $("#mainNav").removeClass("navbar-shrink");
         }
     };
+    */
     // Collapse now if page is not at top
-    navbarCollapse();
+    // navbarCollapse();
     // Collapse the navbar when page is scrolled
     $(window).scroll(navbarCollapse);
 })(jQuery); // End of use strict
