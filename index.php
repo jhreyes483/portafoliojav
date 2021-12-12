3b5fee9bc3cf901ff3b3d9e862f94eb1909391e1
Javier Reyes <jhreyes483@misena.edu.co>
2021-12-11 21:15:23 -0500

    d

--- index.php
+++ index.php
@@ -76,8 +76,9 @@ if( isset ($_POST['a'])){
     <script src="public/vendor/jquery/jquery.min.js"></script>
     <script src="public/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
 
-    <!-- Core plugin JavaScript-->
+    <!-- Core plugin JavaScript
     <script src="public/vendor/jquery-easing/jquery.easing.min.js"></script>
+    -->
 
     <!-- Custom scripts for all pages
     <script src="public/js/sb-admin-2.min.js"></script>
