2a9548dee8aaf20b68989e41f4d83a4ee647ad71
Javier Reyes <jhreyes483@misena.edu.co>
2021-12-21 18:49:27 -0500

    Shore

--- autoload.php
+++ autoload.php
@@ -2,20 +2,16 @@
 
 function autoload($clase){
     $pet = explode('\\',$clase);
-    
     $url = "../".str_replace("\\","/",$clase.".php");
    if( file_exists($url) ){   
        require_once $url; 
    } 
 }
-try{
- //   echo spl_autoload_extensions('.php');
- spl_autoload_register('autoload');
- spl_autoload_extensions('.php');
 
-    
+try{
+ spl_autoload_register('autoload');
 }catch(Exception $e){
-    "-CATCHHH-";
+    echo $e->getMessage();
 }
 
 
