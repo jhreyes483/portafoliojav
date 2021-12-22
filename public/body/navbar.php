cfe593894365cae6dc2100664db11f021d5455c9
Javier Reyes <jhreyes483@misena.edu.co>
2021-12-21 20:42:10 -0500

    Shore

--- public/body/navbar.php
+++ public/body/navbar.php
@@ -25,6 +25,7 @@
                 <a class="dropdown-item" href="../index.php">
                     <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
                     Cerrar Sesion
+                    
                 </a>
             </div>
         </li>
