844a6ba7bc1f3c88e2840227e53dd1d6aff17efb
Javier Reyes <jhreyes483@misena.edu.co>
2021-12-21 19:17:22 -0500

    d

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-define("BASE_URL", "https://portafoliojav.herokuapp.com/");
+define("BASE_URL", "https://portafoliojav.herokuapp.com");
 $URL2 = BASE_URL;
 header("Location:  $URL2/_view/login.php");
 exit();
