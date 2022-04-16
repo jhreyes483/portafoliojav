3f8c6dc7bdd5f14557d8bdaec4d88a36d79238f8
jhreyes483 <jhreyes483@misena.edu.co>
2022-04-16 12:22:39 -0500

    test 1

--- _view/login.php
+++ _view/login.php
@@ -33,7 +33,7 @@ if( isset ($_POST['a'])){
                             <div class="col-lg-6">
                                 <div class="p-5">
                                     <div class="text-center">
-                                        <h1 class="h4 text-gray-900 mb-4">BIENVENIDO A SICLOUD <br> PORTAFOLIO <br>JAVIER REYES</h1>
+                                        <h1 class="h4 text-gray-900 mb-4">BIENVENIDO A SICLOUD. <br> PORTAFOLIO <br>JAVIER REYES</h1>
                                     </div>
                                     <form   class="user" method="post">
                                         <div class="form-group">
